a75d440d5436994d391d6bb4a502856ae7ce77fc
Mark Chavez <mark.chavez@startmail.com>
2021-03-25 22:36:26 -0600

    Removing unnecessary todo

--- server-express-node/application/src/logger.ts
+++ server-express-node/application/src/logger.ts
@@ -3,3 +3,5 @@ import expressPino from 'express-pino-logger';
 
 export const logger = pino({ level: process.env.LOG_LEVEL || 'error' });
 export const expressLogger = expressPino({ logger });
+
+// TODO: Add Logging Helper Functions
