f74b5dc4f2ac8f137263051062a97c5f0617cf05
Mark Chavez <mark.chavez@startmail.com>
2021-06-29 22:12:48 -0600

    Refactor of Client API Results Helpers mixin

--- client-bootstrap-vue/application/src/components/Mixins/apiResultsHelpers.js
+++ client-bootstrap-vue/application/src/components/Mixins/apiResultsHelpers.js
@@ -1,32 +1,63 @@
+import axios from 'axios';
 import { List, Map } from 'immutable';
 import { compose, curry } from 'ramda';
 
+// API URL Variables
+const API_HOST = process.env.VUE_APP_API_HOST || '/';
+const API_PORT = process.env.VUE_APP_API_PORT || 5000;
+const API_PROTOCOL = process.env.VUE_APP_API_PROTOCOL || 'http';
+
+// API URL Helper Methods
+const getApiBaseUrl = () => API_PROTOCOL + '://' + API_HOST + ':' + API_PORT;
+const getFullApiUrl = apiEndpointUrl => getApiBaseUrl() + ('/' == apiEndpointUrl.substring(0,1) ? '' : '/') + apiEndpointUrl;
+
+// API Response Payload Helpers
 const getImmutableApiResults = curry((resultResourceType, apiResults) => List(apiResults.data[resultResourceType].map(Map)));
-const getImmutableAuthorResults = getImmutableApiResults('Authors');
-const getImmutableBookResults = getImmutableApiResults('Books');
-const getImmutableNoteResults = getImmutableApiResults('Notes');
-const getImmutableTagResults = getImmutableApiResults('Tags');
-
 const getFirstResult = resultList => resultList.first();
-const getImmutableFirstAuthorResult = compose(getFirstResult, getImmutableAuthorResults);
-const getImmutableFirstBookResult = compose(getFirstResult, getImmutableBookResults);
-const getImmutableFirstNoteResult = compose(getFirstResult, getImmutableNoteResults);
-const getImmutableFirstTagResult = compose(getFirstResult, getImmutableTagResults);
 
+// Authors API Helpers
+const getImmutableAuthorResults = getImmutableApiResults('Authors');
+const getImmutableFirstAuthorResult = compose(getFirstResult, getImmutableAuthorResults);
+const getAllAuthors = () => axios.get(getFullApiUrl('/authors')).then(getImmutableAuthorResults);
+const getAuthorById = authorId => axios.get(getFullApiUrl('/author/' + authorId)).then(getImmutableFirstAuthorResult);
+
+// Books API Helpers
+const getImmutableBookResults = getImmutableApiResults('Books');
+const getImmutableFirstBookResult = compose(getFirstResult, getImmutableBookResults);
+const getAllBooks = () => axios.get(getFullApiUrl('/books')).then(getImmutableBookResults);
+const getBookById = bookId => axios.get(getFullApiUrl('/book/' + bookId)).then(getImmutableFirstBookResult);
+
+// Notes API Helpers
+const getImmutableNoteResults = getImmutableApiResults('Notes');
+const getImmutableFirstNoteResult = compose(getFirstResult, getImmutableNoteResults);
+const getNotesByBookId = bookId => axios.get('/notes/book/' + bookId).then(getImmutableNoteResults);
+const getNoteById = noteId => axios.get('/note/' + noteId).then(getImmutableFirstNoteResult);
+
+// Tags API Helpers
+const getImmutableTagResults = getImmutableApiResults('Tags');
+const getImmutableFirstTagResult = compose(getFirstResult, getImmutableTagResults);
+const getAllTags = () => axios.get('/tags').then(getImmutableTagResults);
+const getTagById = tagId => axios.get('/tag/' + tagId).then(getImmutableFirstTagResult);
+
+// Browse Resource Sidebar Helpers
 const filterDeleted = item => !item.get('deleted_at');
 const filterNull = item => null != item;
 
 export default {
     methods: {
-        getImmutableAuthorResults,
-        getImmutableBookResults,
-        getImmutableNoteResults,
-        getImmutableTagResults,
+        getFullApiUrl,
 
-        getImmutableFirstAuthorResult,
-        getImmutableFirstBookResult,
-        getImmutableFirstNoteResult,
-        getImmutableFirstTagResult,
+        getAllAuthors,
+        getAuthorById,
+
+        getAllBooks,
+        getBookById,
+
+        getNotesByBookId,
+        getNoteById,
+
+        getAllTags,
+        getTagById,
 
         filterDeleted,
         filterNull,
