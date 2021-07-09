7e0593286a77c3e1e16600c83bb217373a16905d
Mark Chavez <mark.chavez@startmail.com>
2021-07-09 17:02:52 -0600

    Updates to API helpers mixin in client

--- client-bootstrap-vue/application/src/components/Mixins/apiResultsHelpers.js
+++ client-bootstrap-vue/application/src/components/Mixins/apiResultsHelpers.js
@@ -20,24 +20,28 @@ const getImmutableAuthorResults = getImmutableApiResults('Authors');
 const getImmutableFirstAuthorResult = compose(getFirstResult, getImmutableAuthorResults);
 const getAllAuthors = () => axios.get(getFullApiUrl('/authors')).then(getImmutableAuthorResults);
 const getAuthorById = authorId => axios.get(getFullApiUrl('/author/' + authorId)).then(getImmutableFirstAuthorResult);
+const patchAuthorTagById = (authorId, tagId) => axios.patch(getFullApiUrl('/author/' + authorId + '/tag/' + tagId)).then(getImmutableFirstAuthorResult);
+const deleteAuthorTagById = (authorId, tagId) => axios.delete(getFullApiUrl('/author/' + authorId + '/tag/' + tagId)).then(getImmutableFirstAuthorResult);
 
 // Books API Helpers
 const getImmutableBookResults = getImmutableApiResults('Books');
 const getImmutableFirstBookResult = compose(getFirstResult, getImmutableBookResults);
 const getAllBooks = () => axios.get(getFullApiUrl('/books')).then(getImmutableBookResults);
 const getBookById = bookId => axios.get(getFullApiUrl('/book/' + bookId)).then(getImmutableFirstBookResult);
+const patchBookTagById = (bookId, tagId) => axios.patch(getFullApiUrl('/book/' + bookId + '/tag/' + tagId)).then(getImmutableFirstBookResult);
+const deleteBookTagById = (bookId, tagId) => axios.delete(getFullApiUrl('/book/' + bookId + '/tag/' + tagId)).then(getImmutableFirstBookResult);
 
 // Notes API Helpers
 const getImmutableNoteResults = getImmutableApiResults('Notes');
 const getImmutableFirstNoteResult = compose(getFirstResult, getImmutableNoteResults);
-const getNotesByBookId = bookId => axios.get('/notes/book/' + bookId).then(getImmutableNoteResults);
-const getNoteById = noteId => axios.get('/note/' + noteId).then(getImmutableFirstNoteResult);
+const getNotesByBookId = bookId => axios.get(getFullApiUrl('/notes/book/' + bookId)).then(getImmutableNoteResults);
+const getNoteById = noteId => axios.get(getFullApiUrl('/note/' + noteId)).then(getImmutableFirstNoteResult);
 
 // Tags API Helpers
 const getImmutableTagResults = getImmutableApiResults('Tags');
 const getImmutableFirstTagResult = compose(getFirstResult, getImmutableTagResults);
-const getAllTags = () => axios.get('/tags').then(getImmutableTagResults);
-const getTagById = tagId => axios.get('/tag/' + tagId).then(getImmutableFirstTagResult);
+const getAllTags = () => axios.get(getFullApiUrl('/tags')).then(getImmutableTagResults);
+const getTagById = tagId => axios.get(getFullApiUrl('/tag/' + tagId)).then(getImmutableFirstTagResult);
 
 // Validation Helpers
 const validateIdValue = idValueToValidate => {
@@ -60,9 +64,13 @@ export default {
 
         getAllAuthors,
         getAuthorById,
+        patchAuthorTagById,
+        deleteAuthorTagById,
 
         getAllBooks,
         getBookById,
+        patchBookTagById,
+        deleteBookTagById,
 
         getNotesByBookId,
         getNoteById,
