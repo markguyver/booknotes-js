7eddfa9291c99a05d30842a3fe30e6190f0d7a41
Mark Chavez <mark.chavez@startmail.com>
2021-07-11 21:41:58 -0600

    Scaffolding note manipulation buttons on view book page

--- client-bootstrap-vue/application/src/components/PageElements/Books/NotesList.vue
+++ client-bootstrap-vue/application/src/components/PageElements/Books/NotesList.vue
@@ -4,8 +4,8 @@
             <div class="note-item-content" v-html="note.note"></div>
         </div>
         <div class="note-item-buttons">
-            <b-button size="sm" variant="secondary">Edit</b-button>
-            <b-button size="sm" variant="danger">Delete</b-button>
+            <b-button size="sm" variant="secondary" v-on:click="editNote(note.id)">Edit</b-button>
+            <b-button size="sm" variant="danger" v-on:click="deleteNote(note.id)">Delete</b-button>
         </div>
     </div></div>
 </div></template>
@@ -13,6 +13,16 @@
 <script>
 export default {
     name: "NotesList",
+    methods: {
+        deleteNote: function(noteId) {
+            /* eslint no-console: ["error", { allow: ["log", "error"] }] */
+            console.log('Delete Note Clicked, Note ID:', noteId); // TODO: Delete This
+        },
+        editNote: function(noteId) {
+            /* eslint no-console: ["error", { allow: ["log", "error"] }] */
+            console.log('Edit Note Clicked, Note ID:', noteId);
+        },
+    },
     props: ['notes'],
 };
 </script>
@@ -33,8 +43,6 @@ div#notes-list div.booknotes-container div.note-item div.note-item-container {
     border: 1px solid #999999;
     padding: 1rem;
     border-radius: 0.25rem;
-}
-div#notes-list div.booknotes-container div.note-item div.note-item-container div.note-item-content {
 }
 div#notes-list div.booknotes-container div.note-item div.note-item-container div.note-item-content :last-child {
     margin-bottom: 0;
